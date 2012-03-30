323afd8ba95a965c1da9c541f0338bb6716757e2
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2012-03-30 13:18:18 -0400

    work on lab 5

--- extent_client.cc
+++ extent_client.cc
@@ -29,7 +29,6 @@ extent_client::get(extent_protocol::extentid_t eid, std::string &buf)
   
   cache_iterator_t iter = cache.find(eid);
   if (iter == cache.end()) {
-    printf("get fetching %lld into cache\n", eid);
     ret = cl->call(extent_protocol::get, eid, buf);
     if (ret == extent_protocol::OK) {
       cache_entry new_entry;
@@ -60,10 +59,8 @@ extent_client::get(extent_protocol::extentid_t eid, std::string &buf)
     if (iter->second.client_state == cache_entry::REMOVED
         || (iter->second.server_state == cache_entry::SERVER_NOENT
         && iter->second.client_state != cache_entry::WRITTEN)) {
-      printf("get giving NOENT from cache for %lld\n", eid);
       ret = extent_protocol::NOENT;
     } else if (!iter->second.got_data) {
-      printf("get fetching %lld into cache (have an entry but didn't get data before)\n", eid);
       ret = cl->call(extent_protocol::get, eid, buf);
       if (ret == extent_protocol::OK) {
         iter->second.data = buf;
@@ -73,15 +70,13 @@ extent_client::get(extent_protocol::extentid_t eid, std::string &buf)
         iter->second.got_get_attributes = true;
       }
     } else {
-      printf("get returns %lld from cache\n", eid);
       buf = iter->second.data;
       iter->second.attributes.atime = time(NULL);
     }
   }
 
   pthread_mutex_unlock(&mutex);
-  printf("get on %lld returns %d\n", eid, ret);
-  fflush(stdout);
+
   return ret;
 }
 
@@ -124,7 +119,6 @@ extent_client::getattr(extent_protocol::extentid_t eid,
       ret = extent_protocol::NOENT;
     } else if (!iter->second.got_get_attributes
         || !iter->second.got_put_attributes) {
-      printf("getattr: fetching attributes for %lld from server although entry exists. flags %d %d\n", eid, iter->second.got_get_attributes, iter->second.got_put_attributes);
       ret = cl->call(extent_protocol::getattr, eid, attr);
       if (ret == extent_protocol::OK) {
         if (!iter->second.got_get_attributes
@@ -154,10 +148,6 @@ extent_client::getattr(extent_protocol::extentid_t eid,
 
   pthread_mutex_unlock(&mutex);
 
-  //extent_protocol::status ret = extent_protocol::OK;
-  //ret = cl->call(extent_protocol::getattr, eid, attr);
-  printf("getattr on %lld returns %d\n", eid, ret);
-  fflush(stdout);
   return ret;
 }
 
@@ -191,13 +181,8 @@ extent_client::put(extent_protocol::extentid_t eid, std::string buf)
     iter->second.attributes.size = buf.length();
   }
 
-  //int r;
-  //ret = cl->call(extent_protocol::put, eid, buf, r);
-
   pthread_mutex_unlock(&mutex);
 
-  printf("put on %lld returns %d\n", eid, ret);
-  fflush(stdout);
   return ret;
 }
 
@@ -239,23 +224,13 @@ extent_client::remove(extent_protocol::extentid_t eid)
     }
   }
       
-
-  //int r;
-  //ret = cl->call(extent_protocol::remove, eid, r);
-
   pthread_mutex_unlock(&mutex);
-
-  printf("remove on %lld returns %d\n", eid, ret);
-  fflush(stdout);
 
   return ret;
 }
 
 extent_protocol::status
 extent_client::flush(extent_protocol::extentid_t eid) {
-  printf("flushing eid %lld\n", eid);
-  fflush(stdout);
-
   pthread_mutex_lock(&mutex);
 
   extent_protocol::status ret = extent_protocol::OK;
@@ -266,11 +241,9 @@ extent_client::flush(extent_protocol::extentid_t eid) {
         iter->second.server_state != cache_entry::SERVER_NOENT) {
       int r;
       ret = cl->call(extent_protocol::remove, eid, r);
-      printf("flushed remove back for eid %lld, ret %d\n", eid, ret);
     } else if (iter->second.client_state == cache_entry::WRITTEN) {
       int r;
       ret = cl->call(extent_protocol::put, eid, iter->second.data, r);
-      printf("flushed put back for eid %lld, ret %d\n", eid, ret);
     }
 
     cache.erase(iter);
