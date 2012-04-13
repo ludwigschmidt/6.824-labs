49434699ad379983d1a280186734232682bbca34
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2012-04-13 16:12:26 -0400

    Revert "no lock during RPCs"
    This reverts commit cbb0ecd6c9c2a41040f092174d7a28f85270206f.

--- paxos.cc
+++ paxos.cc
@@ -165,16 +165,11 @@ proposer::prepare(unsigned instance, std::vector<std::string> &accepts,
   for (unsigned int ii = 0; ii < nodes.size() && !oldinstance; ++ii) {
     const std::string& cur = nodes[ii];
 
-    VERIFY(pthread_mutex_unlock(&pxs_mutex) == 0);
-    int ret = 0;
     handle h(cur);
     rpcc *cl = h.safebind();
     if (cl != NULL) {
-      ret = cl->call(paxos_protocol::preparereq, me, arg, res, rpcc::to(1000));
-    }
-    VERIFY(pthread_mutex_lock(&pxs_mutex) == 0);
-
-    if (cl != NULL) {
+      int ret =
+          cl->call(paxos_protocol::preparereq, me, arg, res, rpcc::to(1000));
       if (ret == paxos_protocol::OK) {
         if (res.oldinstance) {
           acc->commit(instance, res.v_a);
@@ -216,17 +211,11 @@ proposer::accept(unsigned instance, std::vector<std::string> &accepts,
   for (unsigned int ii = 0; ii < nodes.size(); ++ii) {
     const std::string& cur = nodes[ii];
 
-    VERIFY(pthread_mutex_unlock(&pxs_mutex) == 0);
-    int ret = 0;
     handle h(cur);
     rpcc *cl = h.safebind();
     if (cl != NULL) {
-      ret = cl->call(paxos_protocol::acceptreq, me, arg, accepted,
+      int ret = cl->call(paxos_protocol::acceptreq, me, arg, accepted,
           rpcc::to(1000));
-    }
-    VERIFY(pthread_mutex_lock(&pxs_mutex) == 0);
-
-    if (cl != NULL) {
       if (ret == paxos_protocol::OK) {
         if (accepted) {
           accepts.push_back(cur);
@@ -240,8 +229,6 @@ void
 proposer::decide(unsigned instance, std::vector<std::string> accepts, 
 	      std::string v)
 {
-  // You fill this in for Lab 6
-
   paxos_protocol::decidearg arg;
   arg.instance = instance;
   arg.v = v;
@@ -249,14 +236,12 @@ proposer::decide(unsigned instance, std::vector<std::string> accepts,
   for (unsigned int ii = 0; ii < accepts.size(); ++ii) {
     const std::string& cur = accepts[ii];
 
-    VERIFY(pthread_mutex_unlock(&pxs_mutex) == 0);
     handle h(cur);
     rpcc *cl = h.safebind();
     if (cl != NULL) {
       int r;
       cl->call(paxos_protocol::decidereq, me, arg, r, rpcc::to(1000));
     }
-    VERIFY(pthread_mutex_lock(&pxs_mutex) == 0);
   }
 }
 
--- test_script_lab6.sh
+++ test_script_lab6.sh
@@ -2,8 +2,8 @@
 
 rm -f testlog.txt
 
-for i in `seq 1 30`
+for i in `seq 1 2`
 do
-  ./rsm_tester.pl 0 1 2 3 4 5 6 7 2>&1 >> testlog.txt
+  ./rsm_tester.pl 0 1 2 3 4 5 6 7 >> testlog.txt
   killall lock_server; rm -f *.log
 done
